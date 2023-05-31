3ed23644baef080679a64439549b795e3440133d
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-06-01 03:52:05 +0600

    Completed ESLint, Prettier, Husky, Lint-Staged Setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
-// const port = 5000;
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Successfully!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Successfully!')
+})
 
-export default app;
+export default app
