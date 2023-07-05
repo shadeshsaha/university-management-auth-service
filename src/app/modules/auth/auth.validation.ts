ab409de793011e08848ad7578183d2db96cfca35
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-07-05 19:07:57 +0600

    Added changePasswordZodSchema Validation

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -19,7 +19,19 @@ const refreshTokenZodSchema = z.object({
   }),
 });
 
+const changePasswordZodSchema = z.object({
+  body: z.object({
+    oldPassword: z.string({
+      required_error: 'Old Password Is Required',
+    }),
+    newPassword: z.string({
+      required_error: 'New Password Is Required',
+    }),
+  }),
+});
+
 export const AuthValidation = {
   loginZodSchema,
   refreshTokenZodSchema,
+  changePasswordZodSchema,
 };
